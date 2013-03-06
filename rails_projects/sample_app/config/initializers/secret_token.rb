2a096c131565a9894d48b7241dcd7018f4686b46
RayAbhi2013 <rayabhi2013@gmail.com>
2013-03-06 22:44:17 +0530

    Make a basic User model (including secure passwords)

---
+++ rails_projects/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bcc988c8eb11cc63c5e7b2391d012c0489c19d3df84ff87b0be192e851cef8c8c2a2d2c188572bd4e2663fd1a4fc3763bbbee112f47918ee15f898a915bb50c4'
